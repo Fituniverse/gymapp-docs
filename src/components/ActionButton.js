95294fbf02c872e920fa1c43d421ed9e7d7e5a79
Chesterkxng <cgoita00@gmail.com>
2026-05-16 20:38:39 +0200

    chore: reconduct key added to variants

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -3,6 +3,7 @@ import React from 'react';
 const VARIANTS = {
   edit: { icon: 'boxicons:edit', color: '#5E9C57' },
   delete: { icon: 'line-md:trash', color: '#d9534f' },
+  reconduct: { icon: 'pepicons-pop:arrow-spin', color: '#4972ca' },
   view: { icon: 'mdi:eye', color: '#5bc0de' },
   status: { icon: 'mdi:power', color: '#b593f8c5' },
   key: { icon: 'bitcoin-icons:key-filled', color: '#f0ad4e' },
